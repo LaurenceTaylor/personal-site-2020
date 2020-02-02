bc5d1823e08905ed9d58c51e8068eed06ae1bc5e
Laurence Taylor <ltaylorapp@gmail.com>
2020-02-02 20:56:25 +0000

    feat: random background image

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log('Script loaded');
+const numberOfBackgrounds = 6;
+
+const setBackgroundImage = () => {
+    const html = document.querySelector('html');
+    const randomNumber = Math.floor(Math.random() * numberOfBackgrounds) + 1;
+    html.style.backgroundImage = `url(./assets/backgrounds/${randomNumber}.gif)`
+}
+
+window.onload = () => {
+    setBackgroundImage();
+};
