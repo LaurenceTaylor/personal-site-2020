eb577e08eb6ac1f6062cfff4ee731d19117f6b0e
Laurence Taylor <ltaylorapp@gmail.com>
2020-02-15 15:10:30 +0000

    perf: replace background gifs with mp4s

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 const numberOfBackgrounds = 6;
 
-const setBackgroundImage = () => {
-    const html = document.querySelector('html');
+const setBackground = () => {
+    const video = document.querySelector('video');
     const randomNumber = Math.floor(Math.random() * numberOfBackgrounds) + 1;
-    html.style.backgroundImage = `url(./assets/backgrounds/${randomNumber}.gif)`
+    video.setAttribute('src', `./assets/backgrounds/${randomNumber}.mp4`);
+    // html.style.backgroundImage = `url(./assets/backgrounds/${randomNumber}.gif)`
 }
 
 window.onload = () => {
-    setBackgroundImage();
-};
+    setBackground();
+}
