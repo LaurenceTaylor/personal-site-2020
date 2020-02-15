c74bd227e603e2f79e3f0098b933345f99babbee
Laurence Taylor <ltaylorapp@gmail.com>
2020-02-15 15:34:13 +0000

    chore: clean comments

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ const setBackground = () => {
     const video = document.querySelector('video');
     const randomNumber = Math.floor(Math.random() * numberOfBackgrounds) + 1;
     video.setAttribute('src', `./assets/backgrounds/${randomNumber}.mp4`);
-    // html.style.backgroundImage = `url(./assets/backgrounds/${randomNumber}.gif)`
 }
 
 window.onload = () => {
