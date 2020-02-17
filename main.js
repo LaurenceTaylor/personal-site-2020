292d8b395fa3d9acfd2c24669cbe4698381347e5
Laurence <30178403+LaurenceTaylor@users.noreply.github.com>
2020-02-17 21:04:28 +0000

    feat: recycle button rotates background (#2)

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
-const numberOfBackgrounds = 6;
+const NUMBER_OF_BACKGROUNDS = 6;
 
-const setBackground = () => {
-    const video = document.querySelector('#bg-video');
-    const randomNumber = Math.floor(Math.random() * numberOfBackgrounds) + 1;
-    video.setAttribute('src', `./assets/backgrounds/${randomNumber}.mp4`);
-}
+let backgroundNumber = Math.floor(Math.random() * NUMBER_OF_BACKGROUNDS);
+
+const setBackground = backgroundNumber => {
+  const video = document.querySelector("#bg-video");
+  video.setAttribute("src", `./assets/backgrounds/${backgroundNumber}.mp4`);
+};
+
+const rotateBackground = () => {
+  backgroundNumber = (backgroundNumber + 1) % NUMBER_OF_BACKGROUNDS;
+  setBackground(backgroundNumber);
+};
 
 window.onload = () => {
-    setBackground();
-}
+  setBackground(backgroundNumber);
+
+  const refreshIcon = document.querySelector("#refresh");
+  refreshIcon.onclick = rotateBackground;
+};
